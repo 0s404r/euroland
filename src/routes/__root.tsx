ff1283110c11d6c37e98a004937ad0a18ef3935d
ahmed usama <103342139+0s404r@users.noreply.github.com>
2026-05-22 17:08:08 +0300

    Fix Meta Pixel: inject via dangerouslySetInnerHTML in head

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,7 @@ import '../styles.css'
 
 const META_PIXEL_ID = '4509995009326378'
 
-const metaPixelScript = `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${META_PIXEL_ID}');fbq('track','PageView');`
+const metaPixelScript = "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','" + META_PIXEL_ID + "');fbq('track','PageView');"
 
 export const Route = createRootRoute({
   head: () => ({
@@ -19,9 +19,6 @@ export const Route = createRootRoute({
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@700;800;900&family=Inter:wght@400;500;600;700;800&display=swap' },
     ],
-    scripts: [
-      { children: metaPixelScript },
-    ],
   }),
   shellComponent: RootDocument,
 })
@@ -31,6 +28,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html lang="pt-BR">
       <head>
         <HeadContent />
+        <script dangerouslySetInnerHTML={{ __html: metaPixelScript }} />
       </head>
       <body>
         <noscript>
